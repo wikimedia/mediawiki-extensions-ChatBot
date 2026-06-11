ac068bff051770ae4b9fd44dbdf2a86000e710b2
driedmueller <daniel.riedmueller@hallowelt.com>
2026-06-11 13:27:54 +0200

    Remove deprecated runjobs-trigger-handler. Make cron register more robust
    ERM44313
    [5.3]
    
    Change-Id: If7f66e3c4e943d7123264e181a8abc23cdd6fada

---
+++ src/UpdateLimitsCron.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ChatBot;
+
+use ChatBot\Process\ScheduleLimitUpdate;
+use ExtensionRegistry;
+use MediaWiki\MediaWikiServices;
+use MWStake\MediaWiki\Component\ProcessManager\ManagedProcess;
+use MWStake\MediaWiki\Component\WikiCron\WikiCronManager;
+
+class UpdateLimitsCron {
+
+	/**
+	 * @return void
+	 */
+	public static function register(): void {
+		if ( defined( 'MW_PHPUNIT_TEST' ) || defined( 'MW_QUIBBLE_CI' ) ) {
+			return;
+		}
+
+		if ( !ExtensionRegistry::getInstance()->isLoaded( 'WikiRAG' ) ) {
+			return;
+		}
+
+		/** @var WikiCronManager $cronManager */
+		$cronManager = MediaWikiServices::getInstance()->getService( 'MWStake.WikiCronManager' );
+		$cronManager->registerCron( 'chatbot-update-limits', '0 4 * * *', new ManagedProcess( [
+			'schedule-export' => [
+				'class' => ScheduleLimitUpdate::class,
+				'services' => [ 'WikiRAG.Scheduler' ],
+			]
+		] ) );
+	}
+}
